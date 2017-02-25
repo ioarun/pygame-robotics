31116baf6ca0e2fb7f63820aa3148fba5e5b5808
ioarun <arun_kumar271@hotmail.com>
2017-02-26 01:07:16 +0530

    Add gameover screen and snakehead

--- slither/slither.py
+++ slither/slither.py
@@ -17,37 +17,61 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Slither")
 
+img = pygame.image.load('snake.png')
+
 block_size = 20
 clock = pygame.time.Clock()
 
 FPS = 10
 
-font = pygame.font.SysFont(None, 25)
+direction = "right"
+
+smallfont = pygame.font.SysFont("arial", 25)
+medfont = pygame.font.SysFont("arial", 50)
+largefont = pygame.font.SysFont("arial", 80)
 
 def snake(block_size, snakelist):
-	for x_y in snakelist:
+	if direction == "right":
+		head = pygame.transform.rotate(img, 270)
+	elif direction == "left":
+		head = pygame.transform.rotate(img, 90)
+	elif direction == "up":
+		head = img
+	elif direction == "down":
+		head = pygame.transform.rotate(img, 180)
+
+	gameDisplay.blit(head, (snakelist[-1][0], snakelist[-1][1]))
+	for x_y in snakelist[:-1]:
 		pygame.draw.rect(gameDisplay, green, [x_y[0], x_y[1], block_size, block_size])
 
-def text_objects(text, color):
-	textSurface = font.render(text, True, color)
+def text_objects(text, color, size):
+	if size == "small":	
+		textSurface = smallfont.render(text, True, color)
+	elif size == "medium":
+		textSurface = medfont.render(text, True, color)
+	elif size == "large":
+		textSurface = largefont.render(text, True, color)
+
 	return textSurface, textSurface.get_rect()
 
 
-def message_to_screen(msg, color):
+def message_to_screen(msg, color, y_displace=0, size="small"):
 	# screen_text = font.render(msg,True, color)
 	# gameDisplay.blit(screen_text, [display_width/2, display_height/2])
-	textSurface, textRect = text_objects(msg, color)
-	textRect.center = display_width/2 , display_height/2
+	textSurface, textRect = text_objects(msg, color, size)
+	textRect.center = display_width/2 , display_height/2 + y_displace
 	gameDisplay.blit(textSurface, textRect)
+
 # game loop
 def gameLoop():
+	global direction
 	gameExit = False
 	gameOver = False
 
 	lead_x = display_width/2
 	lead_y = display_height/2
 
-	lead_x_change = 0
+	lead_x_change = 10
 	lead_y_change = 0
 
 	snakelist = []
@@ -63,7 +87,9 @@ def gameLoop():
 
 		while gameOver == True:
 			gameDisplay.fill(white)
-			message_to_screen("Game over, press C to play again or Q to quit.", red)
+			message_to_screen("Game over.", red, y_displace=-50, size="large")
+			message_to_screen("Press C to play again or Q to quit",
+			black, y_displace=50, size="medium")
 			pygame.display.update()
 
 			for event in pygame.event.get():
@@ -82,15 +108,19 @@ def gameLoop():
 				gameExit = True
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
+					direction = "left"
 					lead_x_change = -block_size
 					lead_y_change = 0
 				elif event.key == pygame.K_RIGHT:
+					direction = "right"
 					lead_x_change = block_size
 					lead_y_change = 0
 				elif event.key == pygame.K_UP:
+					direction = "up"
 					lead_y_change = -block_size
 					lead_x_change = 0
 				elif event.key == pygame.K_DOWN:
+					direction = "down"
 					lead_y_change = block_size
 					lead_x_change = 0
 
